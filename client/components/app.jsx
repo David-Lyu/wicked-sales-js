8f40f8ff532b84de9708db866c9aa03d451a054b
David Lyu <lyu.david@yahoo.com>
2020-04-25 10:32:08 -0700

    added two methods for posting and getting to api/cart in app.jsx added a cart icon and its count to header component, created a button that calls to post in cart via props, changed name of props handleclick to setview for productlist component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -7,11 +7,13 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.setView = this.setView.bind(this);
+    this.addToCart = this.addToCart.bind(this);
     this.state = {
       view: {
         name: 'catalog',
         params: {}
-      }
+      },
+      cart: []
     };
   }
 
@@ -24,13 +26,45 @@ export default class App extends React.Component {
     });
   }
 
+  getCartItems() {
+    fetch('/api/cart')
+      .then(results => results.json())
+      .then(data => this.setState({ cart: data }))
+      .catch(err => console.error(err));
+  }
+
+  addToCart(product) {
+    const request = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ productId: product.productId })
+    };
+    fetch('/api/cart', request)
+      .then(results => results.json())
+      .then(data => {
+        const newCart = this.state.cart.concat(data);
+        return this.setState({ cart: newCart });
+      })
+      .catch(err => console.error(err));
+  }
+
+  componentDidMount() {
+    this.getCartItems();
+  }
+
   render() {
     return (
       <div className="container">
-        <Header/>
+        <Header cartItemCount={this.state.cart.length}/>
         {this.state.view.name === 'catalog'
           ? <ProductList setView={this.setView}/>
-          : <ProductDetails setView={this.setView} productId={this.state.view.params}/>
+          : (
+            <ProductDetails setView={this.setView}
+              productId={this.state.view.params}
+              addToCart={this.addToCart}/>
+          )
         }
       </div>
     );
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-export default function Header() {
+export default function Header(props) {
   return (
     <header className="row d-flex align-items-center bg-dark text-white">
-      <i className="fas fa-dollar-sign h2 offset-1"></i>
-      <h1>Wicked Sales</h1>
+      <i className="fas fa-dollar-sign h2 offset-sm-1"></i>
+      <h1 className="col-sm-7">Wicked Sales</h1>
+      <div className="d-flex align-items-center offset-sm-1 col-sm-2 justify-content-center">
+        <h6>{props.cartItemCount + ' items'}</h6>
+        <i className="fas fa-shopping-cart h3"></i>
+      </div>
     </header>
   );
 }
