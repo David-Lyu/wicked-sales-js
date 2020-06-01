d20025df215083ee420c509712f051899db476b9
David Lyu <lyu.david@yahoo.com>
2020-06-01 15:05:50 -0700

    added new lamps and plants picture and added its corresponding changed the index.html file and took out a outer div with class row in the product-list

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,7 +4,7 @@ export default function Header(props) {
   return (
     <header className="row d-flex align-items-center bg-dark text-white">
       <i className="fas fa-dollar-sign h2 offset-sm-1"></i>
-      <h1 className="col-sm-7">Wicked Sales</h1>
+      <h1 className="col-sm-7">Lamp and Plant Store</h1>
       <div onClick={() => { props.setView('cart', {}); }} className="d-flex align-items-center offset-sm-1 col-sm-2 justify-content-center pointer">
         <h6>{props.cartItemCount + ' items'}</h6>
         <i className="fas fa-shopping-cart h3"></i>
