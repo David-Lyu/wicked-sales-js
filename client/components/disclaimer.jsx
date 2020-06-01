d4fca21c26765bea70a0d527ae5b8288455d097b
David Lyu <lyu.david@yahoo.com>
2020-06-01 16:28:49 -0700

    created the modal with boot strap and uses form to validate the acknowledgement and in header created new icon

--- client/components/disclaimer.jsx
+++ client/components/disclaimer.jsx
@@ -3,28 +3,48 @@ import React from 'react';
 export default class Disclaimer extends React.Component {
   constructor(props) {
     super(props);
+    this.handleAcknowledgeClick = this.handleAcknowledgeClick.bind(this);
     this.state = {
-      acknowledgeIsClicked: false
+      showModal: 'block'
     };
   }
 
-  // handleModalLoad(e) {
-  //   e.
-  // }
+  handleAcknowledgeClick(e) {
+    e.preventDefault();
+    this.setState({ showModal: '' });
+  }
 
   render() {
 
     return (
-      <div onLoad={this.handleModalLoad} className="modal">
-        <div className="innerModal">
-          <h1>DisClaimer:</h1>
-          <p>
+      <div className="modal" style={{ display: this.state.showModal }}>
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h3 className="modal-title" id="exampleModalLongTitle">Disclaimer</h3>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <p>
               This site is a demo site and is and will not be used for commercial purposes and cannot make any real purchases
-            <br></br> By checking this box you acknowledge that this site is for demo purposes and that you cannot make any
+                <br></br> By checking this box you acknowledge that this site is for demo purposes and that you cannot make any
               real purchases and will not put any real information when asked for it.
-          </p>
-          <input type="checkbox" required/>
-          <button type="button"></button>
+              </p>
+            </div>
+
+            <div className="modal-footer">
+              <form onSubmit={this.handleAcknowledgeClick} >
+                <label>
+                  I acknowlege
+                  <input type="checkbox" required/>
+                </label>
+                <button>Close</button>
+              </form>
+            </div>
+
+          </div>
         </div>
       </div>
     );
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 export default function Header(props) {
   return (
     <header className="row d-flex align-items-center bg-dark text-white">
-      <i className="fas fa-dollar-sign h2 offset-sm-1"></i>
+      <div className="d-flex justify-content-center">
+        <i className="fas fa-leaf text-success">
+          <i className="fas fa-lightbulb h2 offset-sm-1 text-warning"></i>
+        </i>
+      </div>
       <h1 className="col-sm-7">Lamp and Plant Store</h1>
       <div onClick={() => { props.setView('cart', {}); }} className="d-flex align-items-center offset-sm-1 col-sm-2 justify-content-center pointer">
         <h6>{props.cartItemCount + ' items'}</h6>
