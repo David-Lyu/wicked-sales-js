1bd418f823391f923cac46455dfa339a9f77b8be
David Lyu <lyu.david@yahoo.com>
2020-06-04 16:11:49 -0700

    fixed cart-summary empty cart page to have bg color white and nav tab class fixed header in ipad view to be 100% width, product list added extra padding on bottom to fix overflow issues

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 export default function Header(props) {
   return (
-    <header className="row d-flex align-items-center bg-dark text-white">
+    <header className="row d-flex align-items-center bg-dark text-white w-100">
       <div className="d-flex justify-content-center col-sm-1 offset-sm-1">
         <i className="fas fa-lightbulb h2 logo text-warning pointer" onClick={() => props.setView('catalog')}>
           <i className="fas fa-leaf text-success"></i>
         </i>
       </div>
       <h1 className="col-sm-7">Plant and Lamp Store</h1>
-      <div onClick={() => { props.setView('cart', {}); }} className="d-flex align-items-center offset-sm-1 col-sm-2 justify-content-center pointer">
+      <div onClick={() => { props.setView('cart', {}); }} className="d-flex align-items-center col-sm-3 justify-content-center pointer">
         <h6>{props.cartItemCount + ' items'}</h6>
         <i className="fas fa-shopping-cart h3"></i>
       </div>
